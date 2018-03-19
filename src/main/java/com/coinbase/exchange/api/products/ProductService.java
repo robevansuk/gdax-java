fefe0c8be9c0c466546453e448ab087baad7a2fd
Rob Evans <rwevans1984@gmail.com>
2018-03-19 09:00:04 +0000

    add getHisotricRates request

--- src/main/java/com/coinbase/exchange/api/products/ProductService.java
+++ src/main/java/com/coinbase/exchange/api/products/ProductService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,5 +29,22 @@ public class ProductService {
     // no paged products necessary
     public List<Product> getProducts() {
         return exchange.getAsList(PRODUCTS_ENDPOINT, new ParameterizedTypeReference<Product[]>(){});
+    }
+
+    public List<List<BigDecimal>> getHistoricRates(String productId) {
+        List<Object> response = exchange.getAsList(PRODUCTS_ENDPOINT + "/" + productId + "/candles", new ParameterizedTypeReference<Object[]>() {
+        });
+
+        List<List<BigDecimal>> result = new ArrayList<List<BigDecimal>>();
+        for (Object rate : response) {
+            List iResponse = (List<Object>) rate;
+            List<BigDecimal> recordAsNumbers = new ArrayList<BigDecimal>();
+
+            for (Object r : iResponse) {
+                recordAsNumbers.add(new BigDecimal(r.toString()));
+            }
+            result.add(recordAsNumbers);
+        }
+        return result;
     }
 }
